cc43e3198eabe617cb14ad1931f45f327512a8e0
HerpPotato <adam.a1d9c@gmail.com>
2018-08-17 02:25:08 -0400

    can drag image around added border to image

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,59 +4,119 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
-import javafx.scene.*;
-import javafx.scene.image.Image;
-import javafx.scene.paint.*;
-import javafx.scene.canvas.*;
-
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.Color;
+import javafx.scene.input.MouseEvent;
 import java.awt.image.BufferedImage;
-import java.io.File;
 
 public class Controller
 {
     @FXML Button newButton;
     @FXML Canvas canvas;
 
+    private static boolean mouseHeld = false;
+    private static double mousePressedX, mousePressedY;
+    static int dragOffsetX = 0;
+    static int dragOffsetY = 0;
+
     @FXML private void newButtonPressed()
     {
         ImageInfo imageInfo = new ImageInfo();
-        imageInfo.image = new BufferedImage(800, 600, 5);
-        imageInfo.image.getGraphics().setColor(Color.WHITE);
-        imageInfo.image.getGraphics().drawRect(0, 0, 800, 600);
+        imageInfo.image = new BufferedImage(800, 600, BufferedImage.TYPE_4BYTE_ABGR);
+
+        for (int i = 0; i < 800; i++)
+            for (int j = 0; j < 600; j++)
+                imageInfo.image.setRGB(i, j, 0xFFFFFFFF);
+
         imageInfo.displayImage = new BufferedImage(2 * Main.displayWidth + imageInfo.image.getWidth(),
-                                                   2 * Main.displayHeight + imageInfo.image.getHeight(), 5);
+                                                   2 * Main.displayHeight + imageInfo.image.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
 
-        Main.dragOffsetX = 0;
-        Main.dragOffsetY = 0;
+        for (int i = 0; i < imageInfo.displayImage.getWidth(); i++)
+            for (int j = 0; j < imageInfo.displayImage.getHeight(); j++)
+                imageInfo.displayImage.setRGB(i, j, 0x00000000);
 
-        imageInfo.croppedDisplayImage = imageInfo.displayImage.getSubimage(Main.dragOffsetX, Main.dragOffsetY, Main.displayWidth, Main.displayHeight);
+        for (int i = -2; i < imageInfo.image.getWidth() + 1; i++)
+        {
+            imageInfo.displayImage.setRGB(i + Main.displayWidth, Main.displayHeight - 2, 0xFFD575EA);
+            imageInfo.displayImage.setRGB(i + Main.displayWidth, Main.displayHeight + 1 + imageInfo.image.getHeight(), 0xFFD575EA);
+        }
+
+        for (int i = -2; i < imageInfo.image.getHeight() + 2; i++)
+        {
+            imageInfo.displayImage.setRGB(Main.displayWidth - 2, i + Main.displayHeight, 0xFFD575EA);
+            imageInfo.displayImage.setRGB(Main.displayWidth + 1 + imageInfo.image.getWidth(), i + Main.displayHeight, 0xFFD575EA);
+        }
+
+        //DA9BE8
+
+        for (int i = -1; i < imageInfo.image.getWidth(); i++)
+        {
+            imageInfo.displayImage.setRGB(i + Main.displayWidth, Main.displayHeight - 1, 0xFFDA9BE8);
+            imageInfo.displayImage.setRGB(i + Main.displayWidth, Main.displayHeight + imageInfo.image.getHeight(), 0xFFDA9BE8);
+        }
+
+        for (int i = -1; i < imageInfo.image.getHeight() + 1; i++)
+        {
+            imageInfo.displayImage.setRGB(Main.displayWidth - 1, i + Main.displayHeight, 0xFFDA9BE8);
+            imageInfo.displayImage.setRGB(Main.displayWidth + imageInfo.image.getWidth(), i + Main.displayHeight, 0xFFDA9BE8);
+        }
+
+        imageInfo.displayImage.getGraphics().drawImage(imageInfo.image, Main.displayWidth, Main.displayHeight, null);
+
+        dragOffsetX = 0;
+        dragOffsetY = 0;
+
+        imageInfo.croppedDisplayImage = new BufferedImage(Main.displayWidth, Main.displayHeight, BufferedImage.TYPE_4BYTE_ABGR);
+
+        imageInfo.displayImage.getSubimage(dragOffsetX, dragOffsetY, Main.displayWidth, Main.displayHeight).copyData(imageInfo.croppedDisplayImage.getRaster());
 
         Main.imageInfo = imageInfo;
 
-        // TODO: 8/16/2018 set croppedDisplayImage to UI 
-      
-        try
-        {
-            GraphicsContext gc = canvas.getGraphicsContext2D();
-            Image img = SwingFXUtils.toFXImage(ImageIO.read(new File("res/icon.png")), null);
-            gc.drawImage(img,0,0);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
+        canvas.setWidth(800);
+        canvas.setHeight(600);
+        canvas.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(imageInfo.croppedDisplayImage, null), 0, 0);
     }
 
-    static void dragImage()
+    void dragImage()
     {
-        Main.imageInfo.croppedDisplayImage = Main.imageInfo.displayImage.getSubimage(Main.dragOffsetX, Main.dragOffsetY, Main.displayWidth, Main.displayHeight);
+        Main.imageInfo.croppedDisplayImage.getGraphics().clearRect(0, 0, 800, 600);
+        Main.imageInfo.displayImage.getSubimage(dragOffsetX, dragOffsetY, Main.displayWidth, Main.displayHeight).copyData(Main.imageInfo.croppedDisplayImage.getRaster());
 
-        // TODO: 8/16/2018 set croppedDisplayImage to UI
-        System.out.println("clicky");
+        canvas.getGraphicsContext2D().clearRect(0, 0, Main.displayWidth, Main.displayHeight);
+        canvas.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(Main.imageInfo.croppedDisplayImage, null), 0, 0);
+
+        System.out.println(dragOffsetX + ", " + dragOffsetY);
     }
 
+    @FXML
+    private void mousePressed(MouseEvent e)
+    {
+        mouseHeld = true;
+        mousePressedX = e.getX();
+        mousePressedY = e.getY();
+    }
+
+    @FXML
+    private void mouseDragged(MouseEvent e)
+    {
+        if (mouseHeld)
+        {
+            dragOffsetX -= e.getX() - mousePressedX;
+            dragOffsetY -= e.getY() - mousePressedY;
+            mousePressedX = e.getX();
+            mousePressedY = e.getY();
+
+            if (Main.imageInfo == null || Main.imageInfo.displayImage == null)
+                return;
+
+            dragOffsetX = Math.max(50, Math.min(dragOffsetX, Main.imageInfo.displayImage.getWidth() - Main.displayWidth - 50));
+            dragOffsetY = Math.max(50, Math.min(dragOffsetY, Main.imageInfo.displayImage.getHeight() - Main.displayHeight - 50));
+
+            dragImage();
+        }
+    }
+
+    @FXML
+    private void mouseReleased()
+    {
+        mouseHeld = false;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,12 +3,13 @@ package sample;
 import javafx.application.Application;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.io.File;
 
 public class Main extends Application
@@ -16,47 +17,16 @@ public class Main extends Application
     static ImageInfo imageInfo;
     static int displayWidth = 800;
     static int displayHeight = 600;
-    static int dragOffsetX = 0;
-    static int dragOffsetY = 0;
-
-    private static boolean mouseHeld = false;
-    private static double mousePressedX, mousePressedY;
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("aint");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 706));
         primaryStage.show();
+        primaryStage.setMaximized(true);
 
-        primaryStage.getIcons().add(SwingFXUtils.toFXImage(ImageIO.read(new File("res/icon.png")), null));
-
-        primaryStage.getScene().setOnMousePressed(e ->
-        {
-            mouseHeld = true;
-            mousePressedX = e.getX();
-            mousePressedY = e.getY();
-        });
-
-        primaryStage.getScene().setOnMouseDragged(e ->
-        {
-            if (mouseHeld)
-            {
-                dragOffsetX += e.getX() - mousePressedX;
-                dragOffsetY += e.getY() - mousePressedY;
-                mousePressedX = e.getX();
-                mousePressedY = e.getY();
-
-                if (imageInfo == null || imageInfo.displayImage == null)
-                    return;
-
-                dragOffsetX = Math.max(0, Math.min(dragOffsetX, imageInfo.displayImage.getWidth()));
-                dragOffsetY = Math.max(0, Math.min(dragOffsetY, imageInfo.displayImage.getHeight()));
-            }
-        });
-
-        primaryStage.getScene().setOnMouseReleased((e) -> mouseHeld = false);
         //primaryStage.getScene().getStylesheets().add(Main.class.getResource("res/Stylesheet.css").toExternalForm());
     }
 
